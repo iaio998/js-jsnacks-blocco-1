8f3038b2754879f7da44150e012032eda4505673
iaio998 <depalatisiacopo@gmail.com>
2023-10-13 16:07:24 +0200

    Added bonus on second exercise

--- array_tail/javascript/script.js
+++ array_tail/javascript/script.js
@@ -4,6 +4,7 @@ function getRndInteger(min, max) {
 }
 const btn = document.querySelector("button");
 const inputEl = document.getElementById("data");
+const inputElSecond = document.getElementById("quanti");
 const resultEl = document.querySelector(".alert");
 let msg;
 
@@ -11,9 +12,12 @@ const casualNumb = [];
 
 btn.addEventListener("click", function () {
   inputUser = parseInt(inputEl.value);
+  inputUserSecond = parseInt(inputElSecond.value);
   for (let i = 0; i < inputUser; i++) {
     casualNumb.push(getRndInteger(0, 100));
     console.log(casualNumb);
   }
-  console.log(casualNumb.slice(casualNumb.length - 5, casualNumb.length));
+  console.log(
+    casualNumb.slice(casualNumb.length - inputUserSecond, casualNumb.length)
+  );
 });
