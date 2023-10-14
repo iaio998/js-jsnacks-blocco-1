527667cf55dc494700e10f7b9c563ebbd2f1723c
iaio998 <depalatisiacopo@gmail.com>
2023-10-14 14:59:40 +0200

    Working exercise

--- array_bonus/javascript/script.js
+++ array_bonus/javascript/script.js
@@ -11,12 +11,23 @@ let firstArray = [1, 2, 25, 55, 42, 4];
 let secondArray = [1, 2, 25, 55, 42, 4, 7, 89, 66];
 
 btn.addEventListener("click", function () {
-  for (let i = 0; i < firstArray.length; i++) {
-    let indexFirst = firstArray[i];
-    console.log(indexFirst);
+  console.log(firstArray.length, secondArray.length);
+  if (firstArray.length < secondArray.length) {
+    let diff1 = secondArray.length - firstArray.length;
+    for (let i = 0; i < diff1; i++) {
+      let indexFirst = firstArray[i];
+      firstArray.push(indexFirst);
+    }
+    msg = "Il secondo e' maggiore";
+  } else if (firstArray.length > secondArray.length) {
+    let diff2 = firstArray.length - secondArray.length;
+    for (let a = 0; a < diff2; a++) {
+      let indexSecond = secondArray[a];
+      secondArray.push(indexSecond);
+    }
+    msg = "Il secondo e' minore";
+  } else {
+    msg = "Sono uguali";
   }
-  for (let a = 0; a < secondArray.length; a++) {
-    let indexSecond = secondArray[a];
-    console.log(indexSecond);
-  }
+  console.log(firstArray.length, secondArray.length);
 });
