d83ddf24157fe263131d888aa976cf81363c5b00
iaio998 <depalatisiacopo@gmail.com>
2023-10-14 14:31:44 +0200

    Added for loop on arrays

--- array_bonus/javascript/script.js
+++ array_bonus/javascript/script.js
@@ -6,3 +6,17 @@ const btn = document.querySelector("button");
 const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 let msg;
+
+let firstArray = [1, 2, 25, 55, 42, 4];
+let secondArray = [1, 2, 25, 55, 42, 4, 7, 89, 66];
+
+btn.addEventListener("click", function () {
+  for (let i = 0; i < firstArray.length; i++) {
+    let indexFirst = firstArray[i];
+    console.log(indexFirst);
+  }
+  for (let a = 0; a < secondArray.length; a++) {
+    let indexSecond = secondArray[a];
+    console.log(indexSecond);
+  }
+});
