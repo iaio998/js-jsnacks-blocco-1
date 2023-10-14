79336adce3529b91d38291d79347e3c0a76a77b9
iaio998 <depalatisiacopo@gmail.com>
2023-10-14 15:06:00 +0200

    Fixed bonus

--- array_bonus/javascript/script.js
+++ array_bonus/javascript/script.js
@@ -7,27 +7,26 @@ const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 let msg;
 
-let firstArray = [1, 2, 25, 55, 42, 4];
-let secondArray = [1, 2, 25, 55, 42, 4, 7, 89, 66];
+let firstArray = [1, 2, 25, 66, 12, 44, 67];
+let secondArray = [1, 2, 25, 55];
 
 btn.addEventListener("click", function () {
   console.log(firstArray.length, secondArray.length);
   if (firstArray.length < secondArray.length) {
     let diff1 = secondArray.length - firstArray.length;
     for (let i = 0; i < diff1; i++) {
-      let indexFirst = firstArray[i];
-      firstArray.push(indexFirst);
+      firstArray.push(getRndInteger);
     }
-    msg = "Il secondo e' maggiore";
+    msg = "Il secondo array era piu' lungo";
   } else if (firstArray.length > secondArray.length) {
     let diff2 = firstArray.length - secondArray.length;
     for (let a = 0; a < diff2; a++) {
-      let indexSecond = secondArray[a];
-      secondArray.push(indexSecond);
+      secondArray.push(getRndInteger);
     }
-    msg = "Il secondo e' minore";
+    msg = "Il primo array era piu' lungo";
   } else {
-    msg = "Sono uguali";
+    msg = "I due array sono uguali";
   }
   console.log(firstArray.length, secondArray.length);
+  console.log(msg);
 });
